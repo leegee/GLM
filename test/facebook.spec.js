a015b7187252bf2089edb69afa42711e2ac46a95
Lee Goddard <lee@leegoddard.net>
2016-06-19 15:00:03 +0200

    whitespace

--- test/facebook.spec.js
+++ test/facebook.spec.js
@@ -9,62 +9,62 @@ const Facebook = require('../lib/facebook');
 var config;
 
 describe('Load modules', function () {
-    expect(Facebook).not.to.be.an('undefined');
-    expect(Page).not.to.be.an('undefined');
+	expect(Facebook).not.to.be.an('undefined');
+	expect(Page).not.to.be.an('undefined');
 
-    describe('Config', function () {
-        it('loads', function () {
-            config = require(__dirname + '/../package.json');
-            expect(config).to.be.an('object');
-            expect(config.facebook).to.be.an('object');
-            expect(config.facebook.appId).to.be.a('string');
-        });
-    });
+	describe('Config', function () {
+		it('loads', function () {
+			config = require(__dirname + '/../package.json');
+			expect(config).to.be.an('object');
+			expect(config.facebook).to.be.an('object');
+			expect(config.facebook.appId).to.be.a('string');
+		});
+	});
 });
 
 
 describe('Facebook', function () {
-    var facebook, page;
+	var facebook, page;
 
-    it('should instantiate', function () {
-        expect(Facebook).not.to.be.an('undefined');
-        facebook = new Facebook(config);
-        expect(facebook).to.be.an('object');
-        expect(facebook).to.be.an.instanceof(Facebook);
-        expect(facebook.options).to.be.an('object');
-        expect(facebook.options.facebook).to.be.an('object');
-        expect(facebook.options.facebook.appId).to.be.a('string');
-    });
+	it('should instantiate', function () {
+		expect(Facebook).not.to.be.an('undefined');
+		facebook = new Facebook(config);
+		expect(facebook).to.be.an('object');
+		expect(facebook).to.be.an.instanceof(Facebook);
+		expect(facebook.options).to.be.an('object');
+		expect(facebook.options.facebook).to.be.an('object');
+		expect(facebook.options.facebook.appId).to.be.a('string');
+	});
 
-    it('connects', function (done) {
-        expect(facebook.accessToken).to.be.a('null');
-        var c = facebook.connect();
-        expect(c).to.be.instanceof(Promise);
-        c.then(() => {
-            expect(facebook.accessToken).not.to.be.an('undefined');
-            done();
-        });
-    });
+	it('connects', function (done) {
+		expect(facebook.accessToken).to.be.a('null');
+		var c = facebook.connect();
+		expect(c).to.be.instanceof(Promise);
+		c.then(() => {
+			expect(facebook.accessToken).not.to.be.an('undefined');
+			done();
+		});
+	});
 
-    describe('Page', function () {
-        it('should be instantiate from Facebook', function () {
-            page = facebook.newPage('142326775790907');
-            expect(page).not.to.be.an('undefined');
-            expect(page).to.be.an.instanceof(Page);
-        });
+	describe('Page', function () {
+		it('should be instantiate from Facebook', function () {
+			page = facebook.newPage('142326775790907');
+			expect(page).not.to.be.an('undefined');
+			expect(page).to.be.an.instanceof(Page);
+		});
 
-        it('should fetch from Facebook', (done) => {
-            page.get()
-                .then((result) => {
-                    expect(result).to.be.defined();
-                    console.log(result);
-                    done();
-                })
-                .catch((err) => {
-                    fail();
-                    done();
-                })
-        });
-    });
+		it('should fetch from Facebook', (done) => {
+			page.get()
+				.then((result) => {
+					expect(result).to.be.defined();
+					console.log(result);
+					done();
+				})
+				.catch((err) => {
+					fail();
+					done();
+				})
+		});
+	});
 
 });
